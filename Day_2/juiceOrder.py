c3871f436a4b35dc58016a22bccb134948c5c2f7
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-05-07 17:38:23 +0530

    Create juiceOrder.py

---
+++ Day_2/juiceOrder.py
@@ -0,0 +1,7 @@
+# Order food from a juice shop
+name = input("Enter your name : ")
+location = input("Enter your location details : ")
+print("Available flavours : \nMango\nBanana\nMusk Melon\nApple\nPomegranate")
+flavour = input("Enter the flavour you would like to order : ")
+print("Hey "+name+", your order for "+flavour+" juice is taken :)")
+print("And is out for delivery!!!")
