77afe3f9488830dbc6320afd8ca756e36ea3be15
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-05-07 11:45:15 +0530

    Create dataTypes.py

---
+++ Day_1/dataTypes.py
@@ -0,0 +1,17 @@
+# Telling computer to evaluate an expression
+1 + 5
+# Telling computer to evaluate the expression and show result in the console
+print(1 + 5)
+# integer + integer -> integer
+print(5 + 7)
+# float + float -> float
+print(3.0 + 7.5)
+# integer + float -> float
+print(7 + 6.9)
+# boolean + boolean -> boolean(True-1 & False-0)
+print(True + False)
+# string + string -> string concatenation
+print("Welcome to" + " world of Python")
+# string + number(integer/float) -> Error
+print("hi" + 5)            # TypeError
+print("hello" + 6.7)       # TypeError
