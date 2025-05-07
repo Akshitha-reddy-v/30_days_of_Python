90db3db456ed3d4196aa9266b623d342657e2d6b
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-05-07 14:17:32 +0530

    Create userInput.py

---
+++ Day_2/userInput.py
@@ -0,0 +1,14 @@
+# In Python, when we want to get some data from the user
+# We prompt the user for an input using the function input()
+# Say, we are trying to log into a website.
+# We require username and password as the inputs from user.
+name = input("Please enter your name : ")
+username = input("Enter username : ")
+password = input("Enter password : ")
+print("Hello, "+name+" :)")
+
+# We can store database of a student trying to participate in a sports event
+student_name = input("Enter your name : ")
+grade = input("Enter your grade : ")
+sport = input("Sport you are interested in : ")
+print(student_name + " interested in " + sport + " studying in grade "+grade)
